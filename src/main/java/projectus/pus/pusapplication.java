762bfceb6cec16fa2529f1b5511135885a6d669b
fluencygod <fluencygod@gmail.com>
2022-11-11 20:32:06 +0900

    [초기설정] dependency 추가 및 mysql 연결

---
+++ src/main/java/projectus/pus/pusapplication.java
@@ -0,0 +1,13 @@
+package projectus.pus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class pusapplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(pusapplication.class, args);
+	}
+
+}
---
+++ src/test/java/projectus/pus/pusapplicationTests.java
@@ -0,0 +1,13 @@
+package projectus.pus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class pusapplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
